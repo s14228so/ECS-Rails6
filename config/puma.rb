15e9d765bb0edaae7443f26e9043021e82c4499d
s14228so <saongtx7@gmail.com>
2020-02-04 04:57:16 +0900

    できたああああ！

--- config/puma.rb
+++ config/puma.rb
@@ -6,5 +6,3 @@ plugin :tmp_restart
 
 app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
-
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
